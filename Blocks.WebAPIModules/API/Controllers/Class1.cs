eb560bd7b2399d1d59af6f7326d564bb216e2ed1
stwilson123 <499502394@qq.com>
2017-11-28 19:06:06 +0800

    Test

---
+++ Blocks.WebAPIModules/API/Controllers/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Abp.WebApi.Controllers;
+
+namespace Blocks.WebAPIModules.API.Controllers
+{
+    public class AccountTestController : AbpApiController
+    {
+        [HttpGet]
+        public string Authenticate(string loginModel)
+        {
+            return "hellp";
+
+        }
+    }
+}
