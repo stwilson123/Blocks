68f97c5cc47d7dd52bf4a9af00178f17601b4980
stwilson123 <499502394@qq.com>
2017-12-04 10:37:23 +0800

    ABP

---
+++ Blocks.BussenssWebModule/API/Controllers/AccountTestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Blocks.Framework.Web.Api;
+using Blocks.Framework.Web.Api.Controllers;
+
+namespace Blocks.BussenssWebModule.API.Controllers
+{
+    public class AccountTestController : BlockWebApiController
+    {
+        
+        [HttpGet]
+        public string Get111(string id)
+        {
+            return "hellp";
+
+        }
+    }
+}
