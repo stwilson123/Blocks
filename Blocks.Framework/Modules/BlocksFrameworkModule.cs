7ab567cd117ac3d14dfde345b48e974b03f1da66
stwilson123 <499502394@qq.com>
2017-12-04 17:02:28 +0800

    ABP

---
+++ Blocks.Framework/Modules/BlocksFrameworkModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Abp.Modules;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+
+namespace Blocks.Framework.Modules
+{
+    public class BlocksFrameworkModule: AbpModule
+    {
+        public override void PreInitialize()
+        {
+            //TODO it should be move to more early first Config, but it is belong to Adp
+            IocManager.IocContainer.Kernel.Resolver.AddSubResolver(new CollectionResolver(IocManager.IocContainer.Kernel));
+//            IocManager.Register<WindsorInstanceProvier,WindsorInstanceProvier>();
+//
+//            var iocProvider = IocManager.Resolve<WindsorInstanceProvier>(IocManager);
+//            iocProvider.RegisterKernelCompoentEvnet();
+        }
+
+        public override void Initialize()
+        {
+             
+        }
+    }
+}
