a83ceb8787a75badf51eca0db2a2efd2049b3690
stwilson123 <499502394@qq.com>
2017-12-11 09:24:25 +0800

    ABP

---
+++ Blocks.Web/Modules/Blocks.BussenssWebModule/Models/DefaultModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blocks.BussenssWebModule.Models
+{
+    public class DefaultModel
+    {
+        public int Index { get; set; }
+    }
+}
