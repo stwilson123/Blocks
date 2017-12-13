7ea43aba2ddb87b8eb44e1694e2c4289f251b023
stwilson123 <499502394@qq.com>
2017-12-13 17:13:42 +0800

    ABP

---
+++ Blocks.Web/Modules/Blocks.LayoutModule/ResourceManifestProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Blocks.Framework.Environment.Extensions.Models;
+using Blocks.Framework.Web.Mvc.UI.Resources;
+
+namespace Blocks.LayoutModule {
+    public class ResourceManifestProvider : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            // jQuery.
+            manifest.DefineResource("template","_LayoutFirst").SetUrl("_LayoutFirst.cshtml", "_LayoutFirst.cshtml").SetVersion("0.1");
+
+        }
+
+        public Lazy<FeatureDescriptor> Feature { get; set; }
+    }
+}
