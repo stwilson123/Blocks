be5009d2868f302ffbef6254569d35ee0c52997f
stwilson123 <499502394@qq.com>
2018-01-04 17:02:17 +0800

    ABP

---
+++ EntityFramework.Test/Cache/QueryCacheTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EntityFramework.Test.Cache
+{
+    public class QueryCacheTest
+    {
+        [Fact]
+        public void DoubleSingleOrDefault()
+        {
+            using (var context = new BlocksEntities())
+            {
+                Guid id = Guid.NewGuid();
+                context.TestEntity.SingleOrDefault(t => t.Id == id);
+
+                context.TestEntity.SingleOrDefault(t => t.Id == id);
+
+            }
+        }
+
+
+
+        [Fact]
+        public void DoubleFind()
+        {
+            using (var context = new BlocksEntities())
+            {
+                Guid id = Guid.NewGuid();
+         
+
+                context.TestEntity.Find(id);
+                context.TestEntity.Find(id);
+
+            }
+        }
+    }
+}
---
+++ EntityFramework.Test/TestModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Abp.Modules;
+using Abp.TestBase;
+using Abp.AutoMapper;
+using Blocks;
+
+namespace EntityFramework.Test
+{
+    [DependsOn(
+        typeof(Blocks.Framework.DBORM.BlocksFrameworkDBORMModule),
+        typeof(AbpTestBaseModule),
+        typeof(BlocksDataModule),
+        typeof(AbpAutoMapperModule))]
+    public class TestModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            
+        }
+
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
