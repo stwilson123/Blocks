24233c39cf4d4d232a335caf4bfeb31d18c42227
stwilson123 <499502394@qq.com>
2018-02-24 20:48:39 +0800

    ABP

--- Blocks.Web/Modules/Blocks.ResourcesModule/Framework/UI/UI.js
+++ Blocks.Web/Modules/Blocks.ResourcesModule/Framework/UI/UI.js
@@ -1,8 +1,8 @@
 ﻿; define(["jquery", "slimscroll", "waves", 'sweetalert', 'toastr', 
     '/Modules/Blocks.ResourcesModule/Framework/UI/Component/grid.js', 
-    'bootstrap_select','/Modules/Blocks.ResourcesModule/Framework/UI/Component/msg_alert.js',
-    '/Modules/Blocks.ResourcesModule/Framework/UI/Component/dialog.js'], function ($,scroll,waves,sweetalert,toastr,grid,bootstrap_select,msgAlert) {
+    'bootstrap_select',
+    '/Modules/Blocks.ResourcesModule/Framework/UI/Component/dialog.js'], function ($,scroll,waves,sweetalert,toastr,grid,bootstrap_select,dialog) {
      
 
-        return { 'grid': grid,'message':msgAlert }
+        return { 'grid': grid,'dialog':dialog }
 });
