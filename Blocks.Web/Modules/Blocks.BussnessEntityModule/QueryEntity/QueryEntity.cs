14e277004ca6fc9d28747a0065bce19f86b1acf2
stwilson456 <499502394@qq.com>
2019-05-19 11:30:30 +0800

    Ignore file commint

---
+++ Blocks.Web/Modules/Blocks.BussnessEntityModule/QueryEntity/QueryEntity.cs
@@ -0,0 +1,13 @@
+﻿using Blocks.Framework.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blocks.BussnessEntityModule.QueryEntity
+{
+    public class QueryEntity : IQueryEntity
+    {
+    }
+}
